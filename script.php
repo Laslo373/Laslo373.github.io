4c59306485fb6623d3f0e825f66076de90a8819c
Alexander <43729976+Laslo373@users.noreply.github.com>
2021-06-08 18:01:03 +0400

    Add files via upload

---
+++ script.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 'Hello World!';
+// echo "<script type=\"text/javascript\"> function show_alert() { alert(\"Я здесь\"); } </script>"; 
+
+// $conn_string = "host=92.63.96.147 port=5432 dbname=is user=for_practika password=12345is";
+// $dbconn4 = pg_connect($conn_string);
+
+
+?>
